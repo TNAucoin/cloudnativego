dc3d13834ced0683fa728318df7776fef82a57b8
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-17 22:24:54 -0600

    fixed testing package name conflicts

--- api/resource/book/repository_test.go
+++ api/resource/book/repository_test.go
@@ -1,18 +1,20 @@
-package book
+package book_test
 
 import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	"github.com/tnaucoin/cloudnativego/api/resource/book"
 	mockDB "github.com/tnaucoin/cloudnativego/mock/db"
 	testUtil "github.com/tnaucoin/cloudnativego/util/test"
 	"testing"
+	"time"
 )
 
 func TestRepository_List(t *testing.T) {
 	t.Parallel()
 	db, mock, err := mockDB.NewMockDB()
 	testUtil.NoError(t, err)
-	repo := NewRepository(db)
+	repo := book.NewRepository(db)
 	mockRows := sqlmock.NewRows([]string{"id", "title", "author"}).
 		AddRow(uuid.New(), "Book1", "Author1").
 		AddRow(uuid.New(), "Book2", "Author2")
@@ -24,18 +26,18 @@ func TestRepository_List(t *testing.T) {
 
 func TestRepository_Create(t *testing.T) {
 	t.Parallel()
-
 	db, mock, err := mockDB.NewMockDB()
 	testUtil.NoError(t, err)
-	repo := NewRepository(db)
+
+	repo := book.NewRepository(db)
 	id := uuid.New()
 	mock.ExpectBegin()
 	mock.ExpectExec("^INSERT INTO \"books\" ").
-		WithArgs(id, "Title", "Author", mockDB.AnyTime{}, "", "", mockDB.AnyTime{}, mockDB.AnyTime{}, nil).
+		WithArgs(id, "Title", "Author", mockDB.AnyTime{}, "", "", mockDB.AnyTime{}, mockDB.AnyTime{}, mockDB.AnyTime{}).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
 
-	book := &book
-	_, err = repo.Create(book)
+	b := &book.Book{ID: id, Title: "Title", Author: "Author", PublishedDate: time.Now()}
+	_, err = repo.Create(b)
 	testUtil.NoError(t, err)
 }
